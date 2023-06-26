ffd670d7b254050d07bf13b7a024724531ddf845
dettyboy <dettyboy@icloud.com>
2023-06-26 13:37:27 +0100

    drawer details

---
+++ lib/drawer.dart
@@ -0,0 +1,91 @@
+import 'package:easy_ride/widgets/constants/app_color.dart';
+import 'package:easy_ride/widgets/constants/reusable/text.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CustomDrawer extends StatefulWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color(0xff414141),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 50),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Icon(Iconsax.backward),
+            const SizedBox(
+              height: 10,
+            ),
+            const CircleAvatar(
+              radius: 30,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const CustomText(text: 'Charles Ayomide'),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              'charlesayomide@gmail.com',
+              style: TextStyle(color: AppColor.textColor1),
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: drawerProfile.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Row(
+                          children: [
+                            Icon(
+                              drawerProfile[index]['icon'],
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              drawerProfile[index]['text'],
+                              style:
+                                  const TextStyle(color: AppColor.textColor1),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.white,
+                        height: 11,
+                        thickness: 1,
+                      )
+                    ],
+                  );
+                })
+          ],
+        ),
+      ),
+    );
+  }
+
+  List drawerProfile = [
+    {'icon': Icons.person_2, 'text': 'Edit Profile'},
+    {'icon': Iconsax.location, 'text': 'Address'},
+    {'icon': Iconsax.book, 'text': 'History'},
+    {'icon': Icons.chat, 'text': 'Complain'},
+    {'icon': Icons.person_search, 'text': 'Referral'},
+    {'icon': Icons.share, 'text': 'About Us'},
+    {'icon': Icons.settings, 'text': 'Settings'},
+    {'icon': Icons.help_center, 'text': 'Help and Support'},
+    {'icon': Icons.logout, 'text': 'Logout'}
+  ];
+}
--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -1,17 +1,125 @@
+import 'package:easy_ride/drawer.dart';
+import 'package:easy_ride/widgets/constants/app_color.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      drawer: const CustomDrawer(),
       body: Container(
+        height: double.infinity,
+        width: double.infinity,
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: NetworkImage(
                     'https://res.cloudinary.com/dxje0rp9f/image/upload/v1687770781/easy_ride/Map_wbfwmd.png'),
                 fit: BoxFit.cover)),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 15, right: 15, top: 60, bottom: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: AppColor.backgroundColor,
+                    ),
+                    child: IconButton(
+                        onPressed: () {
+                          return _scaffoldKey.currentState!.openDrawer();
+                        },
+                        icon: const Icon(
+                          Iconsax.menu_1,
+                        )),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: AppColor.backgroundColor,
+                        ),
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Iconsax.search_favorite)),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: AppColor.backgroundColor,
+                        ),
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Iconsax.notification)),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          backgroundColor: AppColor.backgroundColor,
+                          minimumSize: const Size(150, 50)),
+                      onPressed: () {},
+                      child: const Text('Rental')),
+                  const Icon(Icons.navigation_rounded)
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.grey[600],
+                    border: Border.all(color: AppColor.backgroundColor)),
+                child: const Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Iconsax.search_favorite4,
+                            color: Colors.white,
+                          ),
+                          suffixIcon: Icon(
+                            Iconsax.lovely,
+                            color: Colors.white,
+                          ),
+                          hintText: 'Where would you go?',
+                          hintStyle: TextStyle(color: AppColor.textColor1),
+                          enabledBorder: OutlineInputBorder()),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }

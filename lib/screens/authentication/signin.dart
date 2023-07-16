3aebbc299f0fcc5381a67343bdbd11f2de03e62c
dettyboy <dettyboy@icloud.com>
2023-07-16 12:59:38 +0100

    Email and Password initialized with firebase

--- lib/screens/authentication/signin.dart
+++ lib/screens/authentication/signin.dart
@@ -1,9 +1,13 @@
 import 'package:easy_ride/screens/authentication/forgotpassword.dart';
 import 'package:easy_ride/screens/authentication/verification.dart';
+import 'package:easy_ride/widgets/constants/app_color.dart';
 import 'package:easy_ride/widgets/constants/reusable/appbar.dart';
 import 'package:easy_ride/widgets/constants/reusable/elevatedbutton.dart';
 import 'package:easy_ride/widgets/constants/reusable/text.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:easy_ride/firebase_options.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -13,87 +17,126 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 35),
-        child: Column(
-          children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [CustomText(text: 'Sign In')],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: 'Email or Phone Number',
-                  border: OutlineInputBorder()),
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const TextField(
-              decoration: InputDecoration(
-                  hintText: 'Enter Your Password',
-                  border: OutlineInputBorder()),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const ForgotPassword()));
-                    },
-                    child: const Text(
-                      'Forgot your Password?',
-                      style: TextStyle(color: Colors.red),
-                    )),
-              ],
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            CustomElevated(
-              text: 'Sign In',
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const VerificationPage()));
-              },
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Text('or'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: socials
-                  .map((e) => Container(
-                        margin: const EdgeInsets.all(20),
-                        padding: const EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            border: Border.all(color: Colors.white)),
-                        child: Image.network(
-                          e['image'],
-                        ),
-                      ))
-                  .toList(),
-            ),
-            RichText(
-                text: const TextSpan(children: [
-              TextSpan(text: 'Dont Have an account? '),
-              TextSpan(text: ' Sign Up')
-            ])),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 35),
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [CustomText(text: 'Sign In')],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                style: const TextStyle(color: AppColor.textColor1),
+                controller: _email,
+                autocorrect: false,
+                enableSuggestions: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    label: Text('Email or Phone Number'),
+                    labelStyle: TextStyle(color: AppColor.textColor1),
+                    border: OutlineInputBorder()),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              TextField(
+                style: const TextStyle(color: AppColor.textColor1),
+                obscureText: true,
+                autocorrect: false,
+                enableSuggestions: false,
+                controller: _password,
+                decoration: const InputDecoration(
+                    label: Text('Enter Your Password'),
+                    labelStyle: TextStyle(color: AppColor.textColor1),
+                    border: OutlineInputBorder()),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ForgotPassword()));
+                      },
+                      child: const Text(
+                        'Forgot your Password?',
+                        style: TextStyle(color: Colors.red),
+                      )),
+                ],
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomElevated(
+                text: 'Sign In',
+                onPressed: () async {
+                  await Firebase.initializeApp(
+                      options: DefaultFirebaseOptions.currentPlatform);
+                  final email = _email.text;
+                  final password = _password.text;
+                  final UserCredential = await FirebaseAuth.instance
+                      .createUserWithEmailAndPassword(
+                          email: email, password: password);
+                  print(UserCredential);
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (context) => const VerificationPage()));
+                },
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              const Text('or'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: socials
+                    .map((e) => Container(
+                          margin: const EdgeInsets.all(20),
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(color: Colors.white)),
+                          child: Image.network(
+                            e['image'],
+                          ),
+                        ))
+                    .toList(),
+              ),
+              RichText(
+                  text: const TextSpan(children: [
+                TextSpan(text: 'Dont Have an account? '),
+                TextSpan(text: ' Sign Up')
+              ])),
+            ],
+          ),
         ),
       ),
     );

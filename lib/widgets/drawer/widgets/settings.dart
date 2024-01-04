63432535efc2db84361a20cb0bbed2ef84ce85c3
dettyboy <dettyboy@icloud.com>
2024-01-04 23:57:54 +0100

    style: settings done

--- lib/widgets/drawer/widgets/settings.dart
+++ lib/widgets/drawer/widgets/settings.dart
@@ -1,13 +1,23 @@
-import 'package:easy_ride/widgets/constants/reusable/appbar.dart';
 import 'package:flutter/material.dart';
+
+import '../../constants/app_color.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
+    return  Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        backgroundColor: const Color(0xFF121212),
+        title: const Text(
+          'About Us',
+          style: TextStyle(color: AppColor.textColor1),
+        ),
+        centerTitle: true,
+      ),
+      body: const Column(children: []),
     );
   }
 }
